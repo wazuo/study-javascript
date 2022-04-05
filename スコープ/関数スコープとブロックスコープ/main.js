6f71c56e0d0ee48e5203818dbd2757291b472010
wazuo <95359918+wazuo@users.noreply.github.com>
2022-04-05 18:56:40 +0900

    関数スコープとブロックスコープ[finish]

--- スコープ/関数スコープとブロックスコープ/main.js
+++ スコープ/関数スコープとブロックスコープ/main.js
@@ -1,5 +1,17 @@
-function a () {
+function a() {
   let b = 0;
-  console,log(b);
+  console.log(b);
 }
 a();
+
+if(true) {
+  const c = 1;
+  console.log(c);
+
+  const d = function() {
+    console.log('d is called')
+  }
+
+
+  d();
+}
