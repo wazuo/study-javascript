80e7a17c9175ecc49eb9be8e4c9f50aab8d54873
wazuo <95359918+wazuo@users.noreply.github.com>
2022-04-13 11:38:13 +0900

    修了問題クロージャー[end]

--- スコープ/920_修了確認_クロージャー/start/main.js
+++ スコープ/920_修了確認_クロージャー/start/main.js
@@ -20,13 +20,27 @@ function calcFactory(val) {
     plus: function(target) {
       const newVal = val + target;
       console.log(val + " + " + target + " = " + newVal)
+      val = newVal;
     },
     minus: function(target) {
       const newVal = val - target;
       console.log(val + " - " + target + " = " + newVal)
+      val = newVal;
+    },
+    multiply: function(target) {
+      const newVal = val * target;
+      console.log(val + " * " + target + " = " + newVal)
+      val = newVal;
+    },
+    divide: function(target) {
+      const newVal = val / target;
+      console.log(val + " / " + target + " = " + newVal)
+      val = newVal;
     }
   };
 }
 const calc = calcFactory(10);
 calc.plus(5);
 calc.minus(3);
+calc.multiply(3);
+calc.divide(2);
