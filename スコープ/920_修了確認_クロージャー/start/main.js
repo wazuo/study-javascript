1176e683b242df2a5a72d6f87e1b6089254bf7bb
wazuo <95359918+wazuo@users.noreply.github.com>
2022-04-14 17:08:46 +0900

    修了確認即時関数

--- スコープ/920_修了確認_クロージャー/start/main.js
+++ スコープ/920_修了確認_クロージャー/start/main.js
@@ -19,22 +19,22 @@ function calcFactory(val) {
   return {
     plus: function(target) {
       const newVal = val + target;
-      console.log(val + " + " + target + " = " + newVal)
+      console.log(`${val} + ${target} = ${newVal}`);
       val = newVal;
     },
     minus: function(target) {
       const newVal = val - target;
-      console.log(val + " - " + target + " = " + newVal)
+      console.log(`${val} - ${target} = ${newVal}`);
       val = newVal;
     },
     multiply: function(target) {
       const newVal = val * target;
-      console.log(val + " * " + target + " = " + newVal)
+      console.log(`${val} * ${target} = ${newVal}`);
       val = newVal;
     },
     divide: function(target) {
       const newVal = val / target;
-      console.log(val + " / " + target + " = " + newVal)
+      console.log(`${val} / ${target} = ${newVal}`);
       val = newVal;
     }
   };
