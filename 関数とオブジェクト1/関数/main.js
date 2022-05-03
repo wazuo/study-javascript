e75ea1e5e2d90bc49891a439f0e03317b19d09ef
wazuo <95359918+wazuo@users.noreply.github.com>
2022-05-03 23:20:43 +0900

    関数[finish]

--- 関数とオブジェクト1/関数/main.js
+++ 関数とオブジェクト1/関数/main.js
@@ -1,8 +1,9 @@
-function fn(a, b) {
+function fn() {
+    const a = arguments[0];
+    const b = arguments[1];
+    console.log(arguments);
     console.log(a, b);
+    return a;
 }
-function fn(a, b) {
-    console.log(a, b);
-}
-
-fn(1);
+let c = fn(1, undefined);
+console.log(c);
