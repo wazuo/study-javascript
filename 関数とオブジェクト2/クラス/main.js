60f3b7793bd57e92b316818b68b0f21968905a0d
wazuo <95359918+wazuo@users.noreply.github.com>
2022-06-06 11:24:16 +0900

    クラス

---
+++ 関数とオブジェクト2/クラス/main.js
@@ -0,0 +1,23 @@
+class Person{
+  constructor(name, age) {
+    this.name =name;
+    this.age = age;
+  }
+
+  hello() {
+    console.log('hello ' + this.name);
+  }
+}
+
+const bob = new Person('Bob', 23);
+console.log(bob);
+
+// function Person(name, age) {
+//   this.name =name;
+//   this.age = age;
+// }
+
+// Person.prototype.hello = function() {
+//   console.log('hello ' + this.name);
+// }
+
