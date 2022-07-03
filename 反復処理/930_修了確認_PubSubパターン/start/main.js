549e3278646c0a2d41dd6e01b25798750cd4c190
wazuo <95359918+wazuo@users.noreply.github.com>
2022-07-03 10:14:14 +0900

    修了確認PubSubパターン

---
+++ 反復処理/930_修了確認_PubSubパターン/start/main.js
@@ -0,0 +1,22 @@
+/**
+ * 問題：
+ * my-library.jsに記載。
+ */
+
+function customFn1() {
+	console.log('customFn1');
+}
+function customFn2() {
+	console.log('customFn2');
+}
+function customFn3() {
+	console.log('customFn3');
+}
+
+Map, Set
+events.on('beforeInit', customFn1);
+events.off('beforeInit', customFn1);
+events.on('beforeInit', customFn2);
+events.on('afterInit', customFn3);
+
+new MyLibrary();
